e6f32dc85790ccb632efb4b528bcb1836c126b6d
Priyanshu Kumar Singh <priyanshuuu9555@gmail.com>
2026-02-04 10:07:08 +0530

    Fix API routing for honeypot endpoint

--- scamguard/urls.py
+++ scamguard/urls.py
@@ -1,5 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("honeypott.urls")),
+    path("admin/", admin.site.urls),
+    path("api/", include("honeypott.urls")),  # 👈 IMPORTANT
 ]
